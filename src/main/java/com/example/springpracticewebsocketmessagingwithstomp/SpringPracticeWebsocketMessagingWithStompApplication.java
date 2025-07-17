587edd194f709c8354747bf582fd740a706a321d
noobdevsam <mdshoykotislam05@gmail.com>
2025-07-18 01:25:36 +0600

    first commit

---
+++ src/main/java/com/example/springpracticewebsocketmessagingwithstomp/SpringPracticeWebsocketMessagingWithStompApplication.java
@@ -0,0 +1,13 @@
+package com.example.springpracticewebsocketmessagingwithstomp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPracticeWebsocketMessagingWithStompApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPracticeWebsocketMessagingWithStompApplication.class, args);
+    }
+
+}
